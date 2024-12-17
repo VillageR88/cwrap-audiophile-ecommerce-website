c0ec42fb3db89bef4569b8643853da7ac3e1ef67
VillageR88 <villager88pl@gmail.com>
2024-12-18 00:17:33 +0100

    Headphones initial json read template creation
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- static/scripts/headphones.js
+++ static/scripts/headphones.js
@@ -1,42 +1,68 @@
-const categoryContainer = document.getElementById("category-container");
-const categoryContainerWrapper = document.createElement("div");
-const categoryContainerPicture = document.createElement("img");
-const categoryContainerDiv = document.createElement("div");
-const categoryContainerDivNew = document.createElement("span");
-const categoryContainerDivSection = document.createElement("section");
-const categoryContainerDivSectionTitle = document.createElement("h2");
-const categoryContainerDivSectionDescription = document.createElement("p");
-const categoryContainerDivSectionButton = document.createElement("a");
+const data = await fetchJson();
+console.log(data);
 
-categoryContainerWrapper.classList.add("category-container-wrapper");
-categoryContainerPicture.classList.add("category-container-picture");
-categoryContainerDiv.classList.add("category-container-div");
-categoryContainerDivNew.classList.add("category-container-div-new");
-categoryContainerDivSection.classList.add("category-container-div-section");
-categoryContainerDivSectionTitle.classList.add(
-  "category-container-div-section-title"
-);
-categoryContainerDivSectionDescription.classList.add(
-  "category-container-div-section-description"
-);
-categoryContainerDivSectionButton.classList.add(
-  "button1", "category-container-div-section-button"
-);
+for (const item of data.reverse()) {
+  if (item.category === "headphones") {
+    addCategoryProduct(
+      item.categoryImage.desktop.replace("./assets", "../static/images"),
+      item.new,
+      item.name,
+      item.description
+    );
+    console.log(item.categoryImage.desktop.replace("./assets", "../static/images"));
+    console.log(item.new);
+    console.log(item.name);
+    console.log(item.description);
+  }
+}
 
-categoryContainerPicture.src =
-  "static/images/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg";
-categoryContainerDivNew.textContent = "NEW PRODUCT";
-categoryContainerDivSectionTitle.textContent = "XX99 MARK II HEADPHONES";
-categoryContainerDivSectionDescription.textContent =
-  "The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.";
-categoryContainerDivSectionButton.textContent = "SEE PRODUCT";
+async function fetchJson() {
+  const response = await fetch("static/scripts/data.json");
+  const data = await response.json();
+  return data;
+}
 
-categoryContainerWrapper.appendChild(categoryContainerPicture);
-categoryContainerDiv.appendChild(categoryContainerDivNew);
-categoryContainerDiv.appendChild(categoryContainerDivSection);
-categoryContainerDivSection.appendChild(categoryContainerDivSectionTitle);
-categoryContainerDivSection.appendChild(categoryContainerDivSectionDescription);
-categoryContainerDiv.appendChild(categoryContainerDivSectionButton);
-categoryContainerWrapper.appendChild(categoryContainerDiv);
+function addCategoryProduct(imageSrc, newProduct, title, description) {
+  const categoryContainer = document.getElementById("category-container");
+  const categoryContainerWrapper = document.createElement("div");
+  const categoryContainerPicture = document.createElement("img");
+  const categoryContainerDiv = document.createElement("div");
+  const categoryContainerDivNew = document.createElement("span");
+  const categoryContainerDivSection = document.createElement("section");
+  const categoryContainerDivSectionTitle = document.createElement("h2");
+  const categoryContainerDivSectionDescription = document.createElement("p");
+  const categoryContainerDivSectionButton = document.createElement("a");
 
-categoryContainer.appendChild(categoryContainerWrapper);
+  categoryContainerWrapper.classList.add("category-container-wrapper");
+  categoryContainerPicture.classList.add("category-container-picture");
+  categoryContainerDiv.classList.add("category-container-div");
+  categoryContainerDivNew.classList.add("category-container-div-new");
+  categoryContainerDivSection.classList.add("category-container-div-section");
+  categoryContainerDivSectionTitle.classList.add(
+    "category-container-div-section-title"
+  );
+  categoryContainerDivSectionDescription.classList.add(
+    "category-container-div-section-description"
+  );
+  categoryContainerDivSectionButton.classList.add(
+    "button1",
+    "category-container-div-section-button"
+  );
+
+  categoryContainerPicture.src = imageSrc;
+  categoryContainerDivNew.textContent = "NEW PRODUCT";
+  categoryContainerDivSectionTitle.textContent = title?.toUpperCase();
+  categoryContainerDivSectionDescription.textContent = description;
+  categoryContainerDivSectionButton.textContent = "SEE PRODUCT";
+
+  categoryContainerWrapper.appendChild(categoryContainerPicture);
+  if (newProduct) categoryContainerDiv.appendChild(categoryContainerDivNew);
+  categoryContainerDiv.appendChild(categoryContainerDivSection);
+  categoryContainerDivSection.appendChild(categoryContainerDivSectionTitle);
+  categoryContainerDivSection.appendChild(
+    categoryContainerDivSectionDescription
+  );
+  categoryContainerDiv.appendChild(categoryContainerDivSectionButton);
+  categoryContainerWrapper.appendChild(categoryContainerDiv);
+  categoryContainer.appendChild(categoryContainerWrapper);
+}
